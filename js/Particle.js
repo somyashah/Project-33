9aa7cc78cc47c8ac96d54bfff4149cf1e118ae4b
somyashah <76036213+somyashah@users.noreply.github.com>
2021-02-08 12:43:35 +0530

    Add files via upload

---
+++ js/Particle.js
@@ -0,0 +1,41 @@
+class Particle {
+    constructor(x, y,r) {
+
+        var options ={
+            restitution:0.4
+        }
+        this.r=r;
+      
+        this.body = Bodies.circle(x, y, this.r,options);       
+        this.color=color(random(0, 255), random(0, 255), random(0, 255));
+        World.add(world, this.body);
+
+    }
+    display() {
+
+        var pos = this.body.position;
+        var angle = this.body.angle;
+
+        if(particle!=null){
+            particle.display();
+
+            if(particle.body.position.y>760){
+
+                if(particle.body.position.x<300){
+score=score+500;
+particle=null;
+                }
+            }
+        }
+        push();
+        translate(pos.x, pos.y);
+        rotate(angle);
+        //imageMode(CENTER);
+        noStroke();
+        fill(this.color)
+        ellipseMode(RADIUS);
+        ellipse(0, 0, this.r,this.r);
+        pop();
+    }
+
+};
